e7a32cccfd2648387576c2e72b4622af0fe9c953
Mac <Mac@Mac-PC>
2017-06-17 13:58:30 +0800

    20170617

---
+++ web/src/tw/iii/org/jdbc01.java
@@ -0,0 +1,33 @@
+package tw.iii.org;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class jdbc01 {
+
+	public static void main(String[] args) {
+		//1.driver
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("ok");
+		} catch (ClassNotFoundException e) {
+			System.out.println(e);
+			System.exit(0);
+		}
+		//2.connection
+		try {
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ming?" +
+                                   "user=root&password=root");
+			System.out.println("ok");
+		//3.SQL statment
+			Statement stmt = conn.createStatement();
+			String sql = "INSERT INTO cust(cname,tel,birthday) values('ming','123','1994-08-30');";
+			boolean isright=stmt.execute(sql);
+			
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+	}
+}
